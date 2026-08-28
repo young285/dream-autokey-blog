32d84642ae83b18e1512084ce511a448d394ca58
young285 <honeet12@gmail.com>
2026-08-29 06:31:56 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dream-autokey-blog-v2.9';
+const CACHE='dream-autokey-blog-v3.2';
 const CORE=['./admin.html','./quote.html','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./quote-top.png','./quote-bottom.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
